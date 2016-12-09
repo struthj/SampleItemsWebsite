25bd64d3ca2ac763a2b03725b2759943bd92b263
Garrett Smith <garrett.smith@oregonstate.edu>
2016-12-09 15:43:51 -0800

    Templated out moving accessibility selection to JS

---
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Scripts/ItemPage.tsx
@@ -0,0 +1,69 @@
+﻿namespace ItemPage {
+    interface ModalProps {
+        localAccessibility: AccessibilityResourceViewModel[];
+    }
+
+    interface itemViewModel {
+        itemViewerServiceUrl: string;
+        itemDigest: ItemDigest;
+        localAccessibilityViewModel: LocalAccessibilityViewModel;
+    }
+    interface ItemDigest {
+
+    }
+
+    interface LocalAccessibilityViewModel {
+        nonApplicableAccessibilityResources: string;
+        accessibilityResourceViewModels: AccessibilityResourceViewModel[];
+    }
+
+    interface AccessibilityResourceViewModel {
+        defaultCode: string;
+        description: string;
+        disabled: boolean;
+        label: string;
+        selectedCode: string;
+        accessibilityListItems: Selection[];
+    }
+
+    interface Selection {
+        disabled: boolean;
+        selected: boolean;
+        label: string;
+    }
+
+
+    class ItemAccessibilityModal extends React.Component<ModalProps, {}> {
+        
+        constructor() {
+            super();
+        }
+
+        resetOptions() : void {
+            //Clear Cookie
+        }
+
+        saveOptions(): void {
+            //Update Cookie with current options
+        }
+
+        render() {
+            return ({/* Render the modal */});
+        }
+    }
+
+    export class ItemPage extends React.Component<itemViewModel, {}> {
+
+        constructor() {
+            super();
+        }
+
+        render() {
+            return ({/* render the  */});
+        }
+    }
+}
+
+function intializeAccessiblityOptions() {
+
+}
