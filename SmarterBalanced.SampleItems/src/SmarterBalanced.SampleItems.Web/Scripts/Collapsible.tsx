bdd99caf7bbd8c9ce2a9ceee64863baf6c1e56bc
Evan Schallerer <schallee@oregonstate.edu>
2016-12-22 13:34:11 -0800

    removed unnecessary style conditional for Collapsible component

--- SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Scripts/Collapsible.tsx
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Scripts/Collapsible.tsx
@@ -23,8 +23,10 @@
             });
         }
 
-
-        renderWithStyle(body: any) {
+        render() {
+            const body = this.state.isCollapsed
+                ? undefined
+                : this.props.children;
             return (
                 <div className={this.props.className} style={this.props.style}>
                     <label onClick={() => this.toggleState()}>
@@ -33,31 +35,6 @@
                     {body}
                 </div>
             );
-        }
-
-        renderWithoutStyle(body: any) {
-            return (
-                <div className={this.props.className}>
-                    <label onClick={() => this.toggleState()}>
-                        {(this.state.isCollapsed ? "▶ " : "▼ ") + this.props.label}
-                    </label>
-                    {body}
-                </div>
-            );
-        }
-
-        render() {
-            const body = this.state.isCollapsed
-                ? undefined
-                : this.props.children;
-
-            if (this.props.style) {
-                return this.renderWithStyle(body);
-            }
-            else {
-                return this.renderWithoutStyle(body);
-
-            }
         }
     }
 }
