56d9e4e63374a7dadb55b7b58c8e8c59a673c075
Evan Schallerer <schallee@oregonstate.edu>
2016-12-23 13:47:52 -0800

    working expandle nestable items, needs cleanup

--- SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Scripts/Expandable.tsx
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Scripts/Expandable.tsx
@@ -3,29 +3,54 @@
     interface Props {
         label: string;
         body?: JSX.Element;
-        initialState: boolean;
-        children?: NodeComponent[]; 
+        areAllCollapsed: boolean;
+        childNodes?: JSX.Element[];
         className: string;
         style?: React.CSSProperties;
     }
 
     interface State {
         isCollapsed: boolean;
+        childNodes?: JSX.Element[];
     }
 
     export class NodeComponent extends React.Component<Props, State> {
         constructor(props: Props) {
             super(props);
-            console.log(props.initialState);
             this.state = {
-                isCollapsed: props.initialState
+                isCollapsed: props.areAllCollapsed,
+                childNodes: props.childNodes
             };
         }
 
-        toggleState() {
+        componentWillReceiveProps(newProps: Props) {
+            this.toggleAll(newProps.areAllCollapsed);
+        }
+
+        toggleState(isCollapsed: boolean, childNodes?: JSX.Element[]) {
             this.setState({
-                isCollapsed: !this.state.isCollapsed
+                isCollapsed: isCollapsed,
+                childNodes: childNodes
             });
+        }
+
+        toggleAll(isCollapsed: boolean) {
+            let newChildNodes: any = undefined;
+            if (this.state.childNodes != null) {
+                newChildNodes = []
+                for (let i = 0; i < this.state.childNodes.length; i++) {
+                    const child = this.state.childNodes[i];
+                    let props = Object.apply(child.props);
+                    props.areAllCollapsed = isCollapsed;
+                    newChildNodes.push(React.cloneElement(child, props));
+                }
+            }
+            console.log(newChildNodes);
+            this.toggleState(isCollapsed, newChildNodes);
+        }
+
+        toggleCollapse() {
+            this.toggleState(!this.state.isCollapsed, this.state.childNodes);
         }
 
         render() {
@@ -33,25 +58,24 @@
             let style = {};
             if (this.state.isCollapsed) {
                 label = "▶ " + this.props.label;
-                style = {display: "None"}
+                style = { display: "None" }
             }
             else {
                 label = "▼ " + this.props.label;
             }
-
             return (
                 <div className={this.props.className} style={this.props.style}>
-                    <label onClick={() => this.toggleState()}>
+                    <label onClick={() => this.toggleCollapse()}>
                         {label}
                     </label>
                     <div style={style}>
                         {this.props.body}
                         <ul>
-                            {this.props.children}
+                            {this.state.childNodes}
                         </ul>
                     </div>
                 </div>
-                );
+            );
         }
     }
 
--- SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Scripts/TestAboutItem.tsx
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Scripts/TestAboutItem.tsx
@@ -1,7 +1,7 @@
 ﻿interface SampleResponse {
     purpose: string;
     scorePoint: string;
-    name: string; 
+    name: string;
     sampleContent: string;
 }
 
@@ -24,6 +24,7 @@ interface Rubric {
 }
 
 interface AboutItemViewModel {
+    itemKey: number;
     commonCoreStandardsId: string;
     targetId: string;
     grade: GradeLevels;
@@ -35,65 +36,46 @@ interface AboutItemViewModel {
 namespace TestAboutItem {
 
     interface Props {
-        viewModel: AboutItemViewModel;
+        children: JSX.Element[];
     }
 
     interface State {
-        allCollapsed: boolean;
+        isCollapsed: boolean;
+        childNodes: JSX.Element[];
     }
 
     export class AIComponent extends React.Component<Props, State> {
         constructor(props: Props) {
             super(props);
             this.state = {
-                allCollapsed: true
+                isCollapsed: true,
+                childNodes: props.children
+
             };
         }
 
         toggleAll() {
+            const newState = !this.state.isCollapsed;
+            let newChildren: JSX.Element[] = []
+            for (let i = 0; i < this.state.childNodes.length; i++) {
+                const child = this.state.childNodes[i];
+                let props = Object.apply(child.props);
+                props.areAllCollapsed = newState;
+                newChildren.push(React.cloneElement(child, props));
+            }
+
             this.setState({
-                allCollapsed: !this.state.allCollapsed
+                isCollapsed: newState,
+                childNodes: newChildren
             });
         }
 
-
-        renderExpandableElement(
-            label: string,
-            className: string,
-            key: string,
-            body?: JSX.Element,
-            children?: Expandable.NodeComponent[]) {
-            return (
-                <Expandable.NodeComponent
-                    label={label}
-                    body={body}
-                    className={className}
-                    children={children}
-                    initialState={this.state.allCollapsed}
-                    key={key}
-                    />
-            );
-        }
-
-        // TODO...
-        renderChildren() {
-            // TODO: Construct all children
-            const bod = (
-                <div>
-                    <p>some text for fun</p>
-                </div>
-                );
-            return (
-                this.renderExpandableElement("test", "test-elem", "1", bod, undefined)
-                );
-        }
-
         render() {
-            const label = this.state.allCollapsed ? "▶ Show All" : "▼ Hide All";
+            const label = this.state.isCollapsed ? "▶ Show All" : "▼ Hide All";
             return (
                 <div className="more-like-this-container">
                     <button onClick={() => this.toggleAll()}>{label}</button>
-                    {this.renderChildren()}
+                    {this.state.childNodes}
                 </div>
             );
         }
@@ -102,9 +84,168 @@ namespace TestAboutItem {
 
 }
 
+class InitializeAboutItem {
+    rootChildren: JSX.Element[];
+    viewModel: AboutItemViewModel;
+    isCollapsed: boolean = true;
+    className: string;
+
+    constructor(viewModel: AboutItemViewModel, className: string) {
+        this.viewModel = viewModel;
+        this.className = className;
+        this.rootChildren = [];
+
+        this.pushTextElems();
+        this.pushRubrics();
+    }
+
+    renderElements() {
+        ReactDOM.render(
+            <TestAboutItem.AIComponent children={this.rootChildren} />,
+            document.getElementById("more-like-this-container") as HTMLElement
+        );
+    }
+
+    buildTextComponent(key: string, label: string, bodyText: string) {
+        const htmlBody = (<div>{bodyText}</div>);
+        return this.buildGenericComponent(key, label, htmlBody, undefined, undefined);
+    }
+
+    // Helper to build a generic Expandable.NodeComponent
+    buildGenericComponent(
+        key: string,
+        label: string,
+        body?: JSX.Element,
+        style?: React.CSSProperties,
+        childNodes?: JSX.Element[]) {
+        return (
+            <Expandable.NodeComponent
+                key={key}
+                areAllCollapsed={this.isCollapsed}
+                className={this.className}
+                label={label}
+                body={body}
+                style={style}
+                childNodes={childNodes}
+                />
+        );
+    }
+
+    pushTextElems() {
+        this.rootChildren.push(
+            this.buildTextComponent("item-id", "Item Id", this.viewModel.itemKey.toString())
+        );
+
+        this.rootChildren.push(
+            this.buildTextComponent("ccss", "Common Core State Standards", this.viewModel.commonCoreStandardsId)
+        );
+
+        this.rootChildren.push(
+            this.buildTextComponent("target", "Target Id", this.viewModel.targetId)
+        );
+
+
+        const grade = GradeLevels.toString(this.viewModel.grade);
+        this.rootChildren.push(
+            this.buildTextComponent("target-grade", "Target Grade", grade)
+        );
+    }
+
+    buildRubricEntry(rubricEntry: RubricEntry, key: string) {
+        const label = `${rubricEntry.name} (${rubricEntry.scorePoint} points)`;
+        const body = (
+            <div>
+                {rubricEntry.scorePoint} <br />
+                <div dangerouslySetInnerHTML={{ __html: rubricEntry.value }} />
+            </div>
+        );
+        return this.buildGenericComponent(key, label, body, undefined, undefined);
+    }
+
+    buildRubricEntries(rubricEntries: RubricEntry[], parentKey: string) {
+        let rubricsEntryElems: JSX.Element[] = [];
+        if (rubricEntries == null) {
+            return rubricsEntryElems;
+        }
+
+        for (let i = 0; i < rubricEntries.length; i++) {
+            const rubricEntry: RubricEntry = rubricEntries[i];
+            const key = `${parentKey}-${i}`;
+            rubricsEntryElems.push(this.buildRubricEntry(rubricEntry, key));
+        }
+
+        return rubricsEntryElems;
+    }
+
+    buildSampleResponse(sampleResponse: SampleResponse, key: string) {
+        const label = `${sampleResponse.name} (${sampleResponse.scorePoint} Points)`;
+        const body = (
+            <div className="sample-response">
+                <b>{"Purpose: "}</b> {sampleResponse.purpose} <br />
+                <b>{"Sample Response: "}</b><br />
+                <div dangerouslySetInnerHTML={{ __html: sampleResponse.sampleContent }} />
+            </div>
+        );
+        return this.buildGenericComponent(key, label, body, undefined, undefined);
+    }
+
+    buildSampleResponses(sampleResponses: SampleResponse[], parentKey: string) {
+        let sampleResponseElems: JSX.Element[] = [];
+        if (sampleResponses == null) {
+            return sampleResponseElems;
+        }
+
+        for (let i = 0; i < sampleResponses.length; i++) {
+            const sampleResponse = sampleResponses[i];
+            const key = `${parentKey}-${i}`;
+            sampleResponseElems.push(this.buildSampleResponse(sampleResponse, key));
+        }
+        return sampleResponseElems;
+    }
+
+    buildRubricSample(rubricSample: RubricSample, parentKey: string) {
+        const label = `Sample Response (Minimum Score: ${rubricSample.minValue}, Maximum Score ${rubricSample.maxValue})`;
+        const childNodes = this.buildSampleResponses(rubricSample.sampleResponses, parentKey);
+        return this.buildGenericComponent(parentKey, label, undefined, undefined, childNodes);
+    }
+
+    buildRubricSamples(rubricSamples: RubricSample[], parentKey: string) {
+        let rubricsEntryElems: JSX.Element[] = [];
+        if (rubricSamples == null) {
+            return rubricsEntryElems;
+        }
+
+        for (let i = 0; i < rubricSamples.length; i++) {
+            const rubricSample = rubricSamples[i];
+            const key = `${parentKey}-${i}`;
+            rubricsEntryElems.push(this.buildRubricSample(rubricSample, key));
+        }
+        return rubricsEntryElems;
+    }
+
+    buildRubric(rubric: Rubric, idx: string) {
+        const label = `${rubric.language} Rubric`;
+        const key = `${rubric.language}-rubric-${idx}`;
+        const rubricEntries = this.buildRubricEntries(rubric.rubricEntries, key);
+        const rubricSamples = this.buildRubricSamples(rubric.samples, key);
+        
+        return this.buildGenericComponent(key, label, undefined, undefined, rubricEntries.concat(rubricSamples));
+    }
+
+    pushRubrics() {
+        if (this.viewModel.rubrics == null) {
+            return;
+        }
+
+        const rubrics = this.viewModel.rubrics;
+        for (let i = 0; i < rubrics.length; i++) {
+            const rubric = rubrics[i];
+            this.rootChildren.push(this.buildRubric(rubric, i.toString()));
+        }
+    }
+
+}
+
 function initializeExpandable(viewModel: AboutItemViewModel) {
-    ReactDOM.render(
-        <TestAboutItem.AIComponent viewModel={viewModel} />,
-        document.getElementById("more-like-this-container") as HTMLElement
-    );
+    new InitializeAboutItem(viewModel, "about-item-expandable").renderElements();
 }
